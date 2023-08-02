4797ce0ff9d22eae3a3d819978bc4ed938cdf9b7
Kozlow <kozlov.khv@gmail.com>
2023-08-02 21:49:37 +1000

    Test adding funckional to get horoscope on choosen date

--- main.py
+++ main.py
@@ -17,17 +17,16 @@ async def zodiac_sign_list(message: types.Message):
     await message.reply(utils.main_message(), reply_markup=kb.signs_btn)
 
 
+@dp.callback_query_handler(lambda callback: callback.data in SIGN_DICT)
+async def sign_content(callback: types.CallbackQuery):
+    await bot.answer_callback_query(callback.id)
+    await bot.send_message(callback.from_user.id, utils.sign_message(callback), reply_markup=kb.date_btn)
+
+
 @dp.callback_query_handler(lambda callback: callback.data in SING_DATE)
 async def select_date(callback: types.CallbackQuery):
     await bot.answer_callback_query(callback.id)
-    await bot.send_message(callback.from_user.id, f'{callback.data}', reply_markup=kb.date_btn)
-
-
-@dp.callback_query_handler()
-async def sign_content(callback: types.CallbackQuery):
-    await bot.answer_callback_query(callback.id)
-    if callback.data in SIGN_DICT:
-        await bot.send_message(callback.from_user.id, utils.sign_message(callback), reply_markup=kb.date_btn)
+    await bot.send_message(callback.from_user.id, utils.on_date(callback), reply_markup=kb.date_btn)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -15,6 +15,9 @@ class HoroscopeParser:
         return links
 
     def get_horoscope_with_url(self, additional_url):
+        """
+        Функция которая возвращает url гороскопа для выбранного знака зоджиака
+        """
         self.url += additional_url
         return self.get_horoscope()
 
@@ -25,8 +28,19 @@ def main_message():
     return '\n\n'.join([f'{text.today}\n\n{item.text}\n{text.choose_sign}' for item in horoscope_links])
 
 
+save_sign = []
+
+
 def sign_message(callback):
     parser = HoroscopeParser()
+    save_sign.append(callback.data)
     horoscope_links = parser.get_horoscope_with_url(additional_url=f'{callback.data}')
+    sign_value = text.SIGN_DICT.get(callback.data)
+    return '\n\n'.join([f'{text.sign}{sign_value}\n\n{item.text}\n' for item in horoscope_links])
+
+
+def on_date(callback):
+    parser = HoroscopeParser()
+    horoscope_links = parser.get_horoscope_with_url(additional_url=f'{save_sign[-1]}/{callback.data}')
     sign_value = text.SIGN_DICT.get(callback.data)
     return '\n\n'.join([f'{text.sign}{sign_value}\n\n{item.text}\n' for item in horoscope_links])
